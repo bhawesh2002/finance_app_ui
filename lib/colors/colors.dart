47c7f3ff0688d558d79a74c18867bb69b2bd1a9b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-21 16:33:39 +0530

    change the identifiers to match lowerCamelCase

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -1,4 +1,4 @@
 import 'dart:ui';
 
-Color bg_color = const Color(0xFF7C55FE);
-Color card_color = const Color(0xFFBFFF6D);
+Color bgColor = const Color(0xFF7C55FE);
+Color cardColor = const Color(0xFFBFFF6D);
